272eafd8d8c0c5db2f3093689dbf120e2ba6d031
beebra <102611560+AMLETIK09@users.noreply.github.com>
2025-09-14 12:55:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
             statusMessage.innerText = "Отправка данных...";
             statusMessage.style.color = "#555";
             const message = `🎉 Новая заявка на корпоратив:\n\n👤 Имя: ${name}\n✅ Участие: ${attendance}\n👥 Гостей: ${guests}\n💬 Комментарий: ${comment}`;
-            const telegramBotToken = "8042335847:AAG7YW94wZ7Hq7M04S5W-3VPHV1TCGY-zPs";
+            const telegramBotToken = "8256984609:AAGlRQSJNGQTiWFpT1Zmvt2OjSPdrlyHGHA";
             const chatId = "-1002552991233";
             try {
                 const response = await fetch(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
@@ -139,4 +139,5 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.addEventListener('click', startMusic);
         document.body.addEventListener('touchstart', startMusic);
     }
+
 });
